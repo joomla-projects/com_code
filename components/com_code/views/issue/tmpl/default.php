d7bbf555c71afd193d30f230f59f15419286ca97
Michael Babker <mbabker@flbab.com>
2015-02-25 09:04:28 -0500

    Whoops...

--- components/com_code/views/issue/tmpl/default.php
+++ components/com_code/views/issue/tmpl/default.php
@@ -10,7 +10,7 @@
 defined('_JEXEC') or die;
 
 // Load the CSS stylesheets
-JHtml::_('stylesheet', 'com_patchtester/default.css', array(), true);
+JHtml::_('stylesheet', 'com_code/default.css', array(), true);
 ?>
 
 <h1>
