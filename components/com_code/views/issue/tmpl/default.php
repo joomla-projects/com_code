d6eacd48278ea46ab8432a068fccbfbefc53d2f2
Tom Hutchison <Hutchy68@gmail.com>
2015-02-24 23:37:17 -0500

    Adding some containers and elements for a better layout

--- components/com_code/views/issue/tmpl/default.php
+++ components/com_code/views/issue/tmpl/default.php
@@ -14,23 +14,35 @@ defined('_JEXEC') or die;
 	<?php echo $this->item->title; ?>
 </h1>
 
-<?php echo nl2br($this->item->description); ?>
-
-<?php if (!empty($this->tags)) : ?>
-<span>Filed Under:</span>
-<ul>
-<?php foreach ($this->tags as $tag) : ?>
-	<li><?php echo $tag->tag; ?></li>
-<?php endforeach; ?>
-</ul>
-<?php endif; ?>
-
-<?php if (!empty($this->comments)) : ?>
-<span>Responses:</span>
-<?php foreach ($this->comments as $comment) : ?>
-	<div class="well">
-		Posted on <?php echo JHtml::_('date', $comment->created_date, 'j M Y, G:s'); ?> by <?php echo $comment->first_name . ' ' . $comment->last_name; ?>:<br />
-		<?php echo nl2br($comment->body); ?>
+<div id="issue-content">
+	<div class="issue-description">
+		<?php echo nl2br($this->item->description); ?>
 	</div>
-<?php endforeach; ?>
-<?php endif; ?>
+
+	<?php if (!empty($this->tags)) : ?>
+		<div class="issue-tags">
+			<h4>Filed Under:</h4>
+				<ul>
+					<?php foreach ($this->tags as $tag) : ?>
+						<li><?php echo $tag->tag; ?></li>
+					<?php endforeach; ?>
+				</ul>
+		</div>
+	<?php endif; ?>
+
+	<?php if (!empty($this->comments)) : ?>
+		<div class="issue-comments">
+			<h4>Responses:</h4>
+				<?php foreach ($this->comments as $comment) : ?>
+					<div class="issue-comment well">
+						<span class"comment-owner">
+							Posted on <?php echo JHtml::_('date', $comment->created_date, 'j M Y, G:s'); ?> by <?php echo $comment->first_name . ' ' . $comment->last_name; ?>
+						</span>
+						<div class="issue-comment-details">			
+							<?php echo nl2br($comment->body); ?>
+						</div>
+					</div>
+				<?php endforeach; ?>
+		</div>
+	<?php endif; ?>
+</div>
