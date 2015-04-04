093cc6a137ab03eaa9f84a02ae5ff0fbd88bd1cb
Michael Babker <mbabker@flbab.com>
2015-04-04 11:32:50 -0400

    Knock out columns that after conversion we'll stop using

--- administrator/components/com_code/install/updates/4.0.0-2015-03-22.sql
+++ administrator/components/com_code/install/updates/4.0.0-2015-03-22.sql
@@ -18,16 +18,11 @@ CREATE TABLE IF NOT EXISTS `#__code_tracker_issues_temp` (
   `close_by` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `description` mediumtext,
-  `jc_issue_id` int(10) unsigned NOT NULL,
-  `jc_created_by` int(11) NOT NULL,
-  `jc_modified_by` int(11) NOT NULL,
-  `jc_close_by` int(11) NOT NULL,
-  PRIMARY KEY (`issue_id`),
-  UNIQUE KEY `idx_tracker_issues_legacy` (`jc_issue_id`)
+  PRIMARY KEY (`issue_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-INSERT INTO `#__code_tracker_issues_temp` (`issue_id`, `tracker_id`, `status`, `priority`, `created_date`, `created_by`, `modified_date`, `modified_by`, `close_date`, `close_by`, `title`, `description`, `jc_issue_id`, `jc_created_by`, `jc_modified_by`, `jc_close_by`)
-  SELECT `jc_issue_id`, `tracker_id`, `status`, `priority`, `created_date`, `jc_created_by`, `modified_date`, `jc_modified_by`, `close_date`, `jc_close_by`, `title`, `description`, `jc_issue_id`, `jc_created_by`, `jc_modified_by`, `jc_close_by` FROM `#__code_tracker_issues`;
+INSERT INTO `#__code_tracker_issues_temp` (`issue_id`, `tracker_id`, `status`, `priority`, `created_date`, `created_by`, `modified_date`, `modified_by`, `close_date`, `close_by`, `title`, `description`)
+  SELECT `jc_issue_id`, `tracker_id`, `status`, `priority`, `created_date`, `jc_created_by`, `modified_date`, `jc_modified_by`, `close_date`, `jc_close_by`, `title`, `description` FROM `#__code_tracker_issues`;
 
 DROP TABLE `#__code_tracker_issues`;
 RENAME TABLE `#__code_tracker_issues_temp` TO `#__code_tracker_issues`;
@@ -40,15 +35,11 @@ CREATE TABLE IF NOT EXISTS `#__code_tracker_issue_changes_temp` (
   `change_date` datetime NOT NULL,
   `change_by` int(11) NOT NULL,
   `data` text NOT NULL,
-  `jc_change_id` int(10) DEFAULT NULL,
-  `jc_issue_id` int(10) DEFAULT NULL,
-  `jc_change_by` int(10) DEFAULT NULL,
-  PRIMARY KEY (`change_id`),
-  UNIQUE KEY `jc_change_id` (`jc_change_id`)
+  PRIMARY KEY (`change_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-INSERT INTO `#__code_tracker_issue_changes_temp` (`change_id`, `issue_id`, `tracker_id`, `change_date`, `change_by`, `data`, `jc_change_id`, `jc_issue_id`, `jc_change_by`)
-  SELECT `jc_change_id`, `jc_issue_id`, `tracker_id`, `change_date`, `jc_change_by`, `data`, `jc_change_id`, `jc_issue_id`, `jc_change_by` FROM `#__code_tracker_issue_changes`;
+INSERT INTO `#__code_tracker_issue_changes_temp` (`change_id`, `issue_id`, `tracker_id`, `change_date`, `change_by`, `data`)
+  SELECT `jc_change_id`, `jc_issue_id`, `tracker_id`, `change_date`, `jc_change_by`, `data` FROM `#__code_tracker_issue_changes`;
 
 DROP TABLE `#__code_tracker_issue_changes`;
 RENAME TABLE `#__code_tracker_issue_changes_temp` TO `#__code_tracker_issue_changes`;
@@ -61,15 +52,11 @@ CREATE TABLE IF NOT EXISTS `#__code_tracker_issue_commits_temp` (
   `created_date` datetime NOT NULL,
   `created_by` int(11) NOT NULL,
   `message` text NOT NULL,
-  `jc_commit_id` int(10) DEFAULT NULL,
-  `jc_issue_id` int(10) DEFAULT NULL,
-  `jc_created_by` int(10) DEFAULT NULL,
-  PRIMARY KEY (`commit_id`),
-  UNIQUE KEY `jc_commit_id` (`jc_commit_id`)
+  PRIMARY KEY (`commit_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-INSERT INTO `#__code_tracker_issue_commits_temp` (`commit_id`, `issue_id`, `tracker_id`, `created_date`, `created_by`, `message`, `jc_commit_id`, `jc_issue_id`, `jc_created_by`)
-  SELECT `jc_commit_id`, `jc_issue_id`, `tracker_id`, `created_date`, `jc_created_by`, `message`, `jc_commit_id`, `jc_issue_id`, `jc_created_by` FROM `#__code_tracker_issue_commits`;
+INSERT INTO `#__code_tracker_issue_commits_temp` (`commit_id`, `issue_id`, `tracker_id`, `created_date`, `created_by`, `message`)
+  SELECT `jc_commit_id`, `jc_issue_id`, `tracker_id`, `created_date`, `jc_created_by`, `message` FROM `#__code_tracker_issue_commits`;
 
 DROP TABLE `#__code_tracker_issue_commits`;
 RENAME TABLE `#__code_tracker_issue_commits_temp` TO `#__code_tracker_issue_commits`;
@@ -82,15 +69,11 @@ CREATE TABLE IF NOT EXISTS `#__code_tracker_issue_responses_temp` (
   `created_date` datetime NOT NULL,
   `created_by` int(11) NOT NULL,
   `body` text NOT NULL,
-  `jc_response_id` int(10) DEFAULT NULL,
-  `jc_issue_id` int(10) DEFAULT NULL,
-  `jc_created_by` int(10) DEFAULT NULL,
-  PRIMARY KEY (`response_id`),
-  UNIQUE KEY `idx_tracker_responses_legacy` (`jc_response_id`)
+  PRIMARY KEY (`response_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-INSERT INTO `#__code_tracker_issue_responses_temp` (`response_id`, `issue_id`, `tracker_id`, `created_date`, `created_by`, `body`, `jc_response_id`, `jc_issue_id`, `jc_created_by`)
-  SELECT `response_id`, `jc_issue_id`, `tracker_id`, `created_date`, `jc_created_by`, `body`, `jc_response_id`, `jc_issue_id`, `jc_created_by` FROM `#__code_tracker_issue_responses`;
+INSERT INTO `#__code_tracker_issue_responses_temp` (`response_id`, `issue_id`, `tracker_id`, `created_date`, `created_by`, `body`)
+  SELECT `response_id`, `jc_issue_id`, `tracker_id`, `created_date`, `jc_created_by`, `body` FROM `#__code_tracker_issue_responses`;
 
 DROP TABLE `#__code_tracker_issue_responses`;
 RENAME TABLE `#__code_tracker_issue_responses_temp` TO `#__code_tracker_issue_responses`;
@@ -101,12 +84,11 @@ CREATE TABLE IF NOT EXISTS `#__code_tracker_status_temp` (
   `tracker_id` int(10) unsigned NOT NULL,
   `state_id` int(11) DEFAULT NULL,
   `title` varchar(255) NOT NULL,
-  `jc_status_id` int(10) DEFAULT NULL,
   PRIMARY KEY (`status_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-INSERT INTO `#__code_tracker_status_temp`(`status_id`, `tracker_id`, `state_id`, `title`, `jc_status_id`)
-  SELECT `jc_status_id`, `tracker_id`, `state_id`, `title`, `jc_status_id` FROM `#__code_tracker_status`;
+INSERT INTO `#__code_tracker_status_temp`(`status_id`, `tracker_id`, `state_id`, `title`)
+  SELECT `jc_status_id`, `tracker_id`, `state_id`, `title` FROM `#__code_tracker_status`;
 
 DROP TABLE `#__code_tracker_status`;
 RENAME TABLE `#__code_tracker_status_temp` TO `#__code_tracker_status` ;
@@ -116,13 +98,11 @@ CREATE TABLE IF NOT EXISTS `#__code_users_temp` (
   `user_id` int(11) NOT NULL,
   `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
-  `jc_user_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `idx_legacy_user_id` (`jc_user_id`)
+  PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-INSERT INTO `#__code_users_temp`(`user_id`, `first_name`, `last_name`, `jc_user_id`)
-  SELECT `jc_user_id`, `first_name`, `last_name`, `jc_user_id` FROM `#__code_users`;
+INSERT INTO `#__code_users_temp`(`user_id`, `first_name`, `last_name`)
+  SELECT `jc_user_id`, `first_name`, `last_name` FROM `#__code_users`;
 
 DROP TABLE `#__code_users`;
 RENAME TABLE `#__code_users_temp` TO `#__code_users`;
