fa7dcab2352f2eee710b3c4fbc612c73c4c0ebb6
Michael Babker <mbabker@flbab.com>
2016-03-11 16:21:46 -0500

    Enable caching (Fix #20)

--- components/com_code/controller.php
+++ components/com_code/controller.php
@@ -20,4 +20,30 @@ class CodeController extends JControllerLegacy
 	 * @var  string
 	 */
 	protected $default_view = 'trackers';
+
+	/**
+	 * Method to display a view.
+	 *
+	 * @param   boolean  $cachable   If true, the view output will be cached
+	 * @param   array    $urlparams  An array of safe url parameters and their variable types, for valid values see JFilterInput::clean().
+	 *
+	 * @return  $this
+	 *
+	 * @see     JFilterInput::clean()
+	 */
+	public function display($cachable = false, $urlparams = [])
+	{
+		$cachable  = JFactory::getUser()->guest;
+		$urlparams = [
+			'search'     => 'STRING',
+			'limit'      => 'UINT',
+			'limitstart' => 'UINT',
+			'tracker_id' => 'UINT',
+			'issue_id'   => 'UINT',
+			'lang'       => 'STRING',
+			'Itemid'     => 'UINT'
+		];
+
+		return parent::display($cachable, $urlparams);
+	}
 }
